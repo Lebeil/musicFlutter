2f81b146d9f06c9899cda8b532ab7ed29952a41f
Lebeil <liedel75@gmail.com>
2021-09-22 21:16:33 +0200

    add images & class musique

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'musique.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ lib/musique.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Musique {
+
+  String titre;
+  String artiste;
+  String imagePath;
+  String urlSong;
+
+  Musique(this.titre, this.artiste, this.imagePath, this.urlSong);
+
+}
